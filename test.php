a21fd7ab1ca1b966d26921c7a31dd34560773c31
hcgcarry <hcgcarry@gmail.com>
2018-06-30 13:06:19 +0800

    carry

---
+++ test.php
@@ -0,0 +1,18 @@
+<HTML>
+<BODY>
+<h2 ALIGN="CENTER"><img border="0" src="login.gif"></h2>
+<CENTER>
+嗨！親愛的會員您好，為讓您能在本站通行無阻，<BR>
+請於下表填入您的認證資料！
+<FORM Action="<?=$_SERVER["PHP_SELF"];?>" Method=post>
+<TABLE BORDER=1 CELLSPACING=0 >
+<TR><TD ALIGN=RIGHT>登入名稱:</TD>
+<TD><Input Type=Text Name=ID Size=10></TD></TR>
+<TR><TD ALIGN=RIGHT>登入密碼:</TD>
+<TD><Input Type=Password Name=Password Size=8></TD></TR>
+</TABLE><P>
+<INPUT Type=Submit Value=" 確 定 " name="B1">
+</FORM>
+</CENTER>
+</BODY>
+</HTML>
